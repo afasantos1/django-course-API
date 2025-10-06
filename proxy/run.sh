e40689f3fe7020a794fd7c9568f3c1b18b5c9075
afasantos1 <afasantosu@gmail.com>
2025-10-06 16:07:20 +0100

    End of course

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-envsubst < /etc/nginx/default.conf.tpl > /tmp/default.conf
-nginx -c /tmp/default.conf -g 'daemon off;'
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
